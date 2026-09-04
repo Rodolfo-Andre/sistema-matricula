bfb9b1f639e146ffe98578fbb3c3cddc2feb0c22
Jayson-Developer <69487783+Jayson-Developer@users.noreply.github.com>
2026-09-03 19:18:41 -0500

    Primer commit: estructura inicial del sistema de matricula

---
+++ Sistema_Matricula/src/main/java/com/matricula/sistema_matricula/Sistema_Matricula.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.matricula.sistema_matricula;
+
+/**
+ *
+ * @author hans5
+ */
+public class Sistema_Matricula {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
