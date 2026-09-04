2c87a9b3af6cfee55707df967985b4333e66d264
Nvidia2004 <orlandogeminis27@gmail.com>
2026-09-03 21:02:53 -0500

    Agrega clase Curso con codigo,nombre y creditos

---
+++ Curso.java
@@ -0,0 +1,52 @@
+/**
+ * Representa un curso del sistema de matricula.
+ * Modulo: Gestion de Cursos.
+ */
+public class Curso {
+
+    private String codigo;
+    private String nombre;
+    private int creditos;
+
+    public Curso(String codigo, String nombre, int creditos) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.creditos = creditos;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    /**
+     * Muestra por consola los datos del curso.
+     */
+    public void mostrarDatos() {
+        System.out.printf("Codigo: %-8s Nombre: %-25s Creditos: %d%n", codigo, nombre, creditos);
+    }
+
+    @Override
+    public String toString() {
+        return codigo + " - " + nombre + " (" + creditos + " creditos)";
+    }
+}
