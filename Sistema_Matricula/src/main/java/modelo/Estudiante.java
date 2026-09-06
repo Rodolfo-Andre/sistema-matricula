8460e5d2329aec071d4ad5fca05645ee243b14bb
Jayson-Developer <69487783+Jayson-Developer@users.noreply.github.com>
2026-09-06 15:12:43 -0500

    Cambio para Prueba - toString -Codigos
    Prueba de conflicto

--- Sistema_Matricula/src/main/java/modelo/Estudiante.java
+++ Sistema_Matricula/src/main/java/modelo/Estudiante.java
@@ -36,7 +36,7 @@ public class Estudiante {
 
     @Override
     public String toString() {
-        return String.format("Código: %s | Nombre: %s | Carrera: %s | Ciclo: %d",
+        return String.format("Códigos: %s | Nombre: %s | Carrera: %s | Ciclo: %d",
                 codigo, nombre, carrera, ciclo);
     }
 }
